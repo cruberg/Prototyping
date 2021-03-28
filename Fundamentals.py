fd7281d2eee524aebfeeed3027c4516949fb50a9
cruberg <cruberg@protonmail.ch>
2021-03-28 11:01:04 -0500

    Initial commit

---
+++ Fundamentals.py
@@ -0,0 +1,14 @@
+Fundamentals = {
+    "AAPL": {
+        "overvalued": -1,
+        "undervalued": -1
+    },
+    "TSLA": {
+        "overvalued": -1,
+        "undervalued": -1
+    },
+    "MSFT": {
+        "overvalued": -1,
+        "undervalued": -1
+    }
+}
